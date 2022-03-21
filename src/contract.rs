00b83731c4c653c7263e19239bcdd54d62d253dd
Pierce Trey <ptrey@figure.com>
2022-03-21 16:54:58 -0600

    some scope match tests
    - waiting on MetadataMsgParams to be exposed from provwasm for one test

--- src/contract.rs
+++ src/contract.rs
@@ -1263,7 +1263,7 @@ mod tests {
     }
 
     #[test]
-    fn execute_with_valid_data() {
+    fn execute_match_with_valid_coin_data() {
         // setup
         let mut deps = mock_dependencies(&[]);
         if let Err(error) = set_contract_info(
@@ -1345,7 +1345,116 @@ mod tests {
     }
 
     #[test]
-    fn execute_with_invalid_data() {
+    fn execute_match_with_valid_scope_data() {
+        // setup
+        let mut deps = mock_dependencies(&[]);
+
+        let scope_input = Scope {
+            scope_id: "scope1234".to_string(),
+            specification_id: "scopespec1".to_string(),
+            owners: vec![Party {
+                address: Addr::unchecked("asker"),
+                role: PartyType::Owner,
+            }],
+            data_access: vec![],
+            value_owner_address: Addr::unchecked("asker"), // todo: does this need to be the contract's address?
+        };
+        deps.querier.with_scope(scope_input.clone());
+
+        if let Err(error) = set_contract_info(
+            &mut deps.storage,
+            &ContractInfo::new(
+                Addr::unchecked("contract_admin"),
+                "contract_bind_name".into(),
+                "contract_name".into(),
+            ),
+        ) {
+            panic!("unexpected error: {:?}", error)
+        }
+
+        // store valid ask order
+        let ask_order = AskOrderV2 {
+            base: BaseType::scope(&scope_input.scope_id),
+            id: "ask_id".into(),
+            owner: Addr::unchecked("asker"),
+            quote: coins(200, "quote_1"),
+        };
+
+        let mut ask_storage = get_ask_storage_v2(&mut deps.storage);
+        if let Err(error) = ask_storage.save(ask_order.id.as_bytes(), &ask_order) {
+            panic!("unexpected error: {:?}", error)
+        };
+
+        // store valid bid order
+        let bid_order = BidOrderV2 {
+            base: BaseType::scope(scope_input.scope_id),
+            effective_time: Some(Timestamp::default()),
+            id: "bid_id".to_string(),
+            owner: Addr::unchecked("bidder"),
+            quote: coins(200, "quote_1"),
+        };
+
+        let mut bid_storage = get_bid_storage_v2(&mut deps.storage);
+        if let Err(error) = bid_storage.save(bid_order.id.as_bytes(), &bid_order) {
+            panic!("unexpected error: {:?}", error);
+        };
+
+        // execute on matched ask order and bid order
+        let execute_msg = ExecuteMsg::ExecuteMatch {
+            ask_id: ask_order.id,
+            bid_id: bid_order.id.clone(),
+        };
+
+        let contract_env = mock_env();
+        let execute_response = execute(
+            deps.as_mut(),
+            contract_env,
+            mock_info("contract_admin", &[]),
+            execute_msg,
+        );
+
+        // validate execute response
+        match execute_response {
+            Err(error) => panic!("unexpected error: {:?}", error),
+            Ok(execute_response) => {
+                assert_eq!(execute_response.attributes.len(), 1);
+                assert_eq!(execute_response.attributes[0], attr("action", "execute"));
+                assert_eq!(execute_response.messages.len(), 2);
+                assert_eq!(
+                    execute_response.messages[0].msg,
+                    CosmosMsg::Bank(BankMsg::Send {
+                        to_address: ask_order.owner.to_string(),
+                        amount: ask_order.quote,
+                    })
+                );
+                handle_expected_scope(&bid_order.base, |scope_id| {
+                    if let CosmosMsg::Custom(ProvenanceMsg { params, .. }) =
+                        &execute_response.messages[1].msg
+                    {
+                        todo!("Uncomment this once MetadataMsgParams are exposed, and hopefully it just works :magic:");
+                        // assert_eq!(
+                        //     params,
+                        //     ProvenanceMsgParams::Metadata(MetadataMsgParams::WriteScope {
+                        //         scope: Scope {
+                        //             scope_id: scope_input.scope_id,
+                        //             specification_id: scope_input.specification_id,
+                        //             owners: scope_input.owners,
+                        //             data_access: scope_input.data_access,
+                        //             value_owner_address: bid_order.owner
+                        //         },
+                        //         signers: vec![contract_env.contract.address]
+                        //     }),
+                        // );
+                    } else {
+                        panic!("Unexpected second message type for match, expected WriteScope, received {:?}", execute_response.messages[1].msg)
+                    }
+                });
+            }
+        }
+    }
+
+    #[test]
+    fn execute_match_with_invalid_coin_data() {
         // setup
         let mut deps = mock_dependencies(&[]);
         if let Err(error) = set_contract_info(
@@ -1373,6 +1482,157 @@ mod tests {
         };
 
         // store valid bid order
+        let bid_order = BidOrderV2 {
+            base: BaseType::coin(100, "base_1"),
+            effective_time: Some(Timestamp::default()),
+            id: "bid_id".into(),
+            owner: Addr::unchecked("bidder"),
+            quote: coins(100, "quote_1"),
+        };
+
+        let mut bid_storage = get_bid_storage_v2(&mut deps.storage);
+        if let Err(error) = bid_storage.save(bid_order.id.as_bytes(), &bid_order) {
+            panic!("unexpected error: {:?}", error);
+        };
+
+        // execute by non-admin ContractError::Unauthorized
+        let execute_msg = ExecuteMsg::ExecuteMatch {
+            ask_id: "ask_id".into(),
+            bid_id: "bid_id".into(),
+        };
+
+        let execute_response = execute(
+            deps.as_mut(),
+            mock_env(),
+            mock_info("user", &[]),
+            execute_msg,
+        );
+
+        match execute_response {
+            Err(ContractError::Unauthorized {}) => {}
+            Err(error) => panic!("unexpected error: {:?}", error),
+            Ok(_) => panic!("expected error, but execute_response ok"),
+        }
+
+        // execute on mismatched ask order and bid order returns ContractError::AskBidMismatch
+        let execute_msg = ExecuteMsg::ExecuteMatch {
+            ask_id: "ask_id".into(),
+            bid_id: "bid_id".into(),
+        };
+
+        let execute_response = execute(
+            deps.as_mut(),
+            mock_env(),
+            mock_info("contract_admin", &[]),
+            execute_msg,
+        );
+
+        match execute_response {
+            Err(ContractError::AskBidMismatch {}) => {}
+            Err(error) => panic!("unexpected error: {:?}", error),
+            Ok(_) => panic!("expected error, but execute_response ok"),
+        }
+
+        // execute on non-existent ask order and bid order returns ContractError::AskBidMismatch
+        let execute_msg = ExecuteMsg::ExecuteMatch {
+            ask_id: "no_ask_id".into(),
+            bid_id: "bid_id".into(),
+        };
+
+        let execute_response = execute(
+            deps.as_mut(),
+            mock_env(),
+            mock_info("contract_admin", &[]),
+            execute_msg,
+        );
+
+        match execute_response {
+            Err(ContractError::AskBidMismatch {}) => {}
+            Err(error) => panic!("unexpected error: {:?}", error),
+            Ok(_) => panic!("expected error, but execute_response ok"),
+        }
+
+        // execute on non-existent ask order and bid order returns ContractError::AskBidMismatch
+        let execute_msg = ExecuteMsg::ExecuteMatch {
+            ask_id: "ask_id".into(),
+            bid_id: "no_bid_id".into(),
+        };
+
+        let execute_response = execute(
+            deps.as_mut(),
+            mock_env(),
+            mock_info("contract_admin", &[]),
+            execute_msg,
+        );
+
+        match execute_response {
+            Err(ContractError::AskBidMismatch {}) => {}
+            Err(error) => panic!("unexpected error: {:?}", error),
+            Ok(_) => panic!("expected error, but execute_response ok"),
+        }
+
+        // execute with sent_funds returns ContractError::ExecuteWithFunds
+        let execute_msg = ExecuteMsg::ExecuteMatch {
+            ask_id: "ask_id".into(),
+            bid_id: "bid_id".into(),
+        };
+
+        let execute_response = execute(
+            deps.as_mut(),
+            mock_env(),
+            mock_info("contract_admin", &coins(100, "funds")),
+            execute_msg,
+        );
+
+        match execute_response {
+            Err(ContractError::ExecuteWithFunds {}) => {}
+            Err(error) => panic!("unexpected error: {:?}", error),
+            Ok(_) => panic!("expected error, but execute_response ok"),
+        }
+    }
+
+    #[test]
+    fn execute_match_with_invalid_scope_data() {
+        // setup
+        let mut deps = mock_dependencies(&[]);
+
+        let scope_input = Scope {
+            scope_id: "scope1234".to_string(),
+            specification_id: "scopespec1".to_string(),
+            owners: vec![Party {
+                address: Addr::unchecked("asker"),
+                role: PartyType::Owner,
+            }],
+            data_access: vec![],
+            value_owner_address: Addr::unchecked("asker"), // todo: does this need to be the contract's address?
+        };
+        deps.querier.with_scope(scope_input.clone());
+
+        if let Err(error) = set_contract_info(
+            &mut deps.storage,
+            &ContractInfo::new(
+                Addr::unchecked("contract_admin"),
+                "contract_bind_name".into(),
+                "contract_name".into(),
+            ),
+        ) {
+            panic!("unexpected error: {:?}", error)
+        }
+
+        // store valid ask order
+        let ask_order = AskOrderV2 {
+            base: BaseType::scope(scope_input.scope_id),
+            id: "ask_id".into(),
+            owner: Addr::unchecked("asker"),
+            quote: coins(100, "quote_1"),
+        };
+
+        let mut ask_storage = get_ask_storage_v2(&mut deps.storage);
+        if let Err(error) = ask_storage.save(ask_order.id.as_bytes(), &ask_order) {
+            panic!("unexpected error: {:?}", error)
+        };
+
+        // store invalid bid order
         let bid_order = BidOrderV2 {
             base: BaseType::coin(100, "base_1"),
             effective_time: Some(Timestamp::default()),
@@ -1565,6 +1825,13 @@ mod tests {
         match base_type {
             BaseType::Coin { coins } => action(coins),
             _ => panic!("Unexpected base type of scope"),
+        }
+    }
+
+    fn handle_expected_scope<A: FnOnce(&String) -> ()>(base_type: &BaseType, action: A) {
+        match base_type {
+            BaseType::Scope { scope_address } => action(scope_address),
+            _ => panic!("Unexpected base type of coin"),
         }
     }
 }
